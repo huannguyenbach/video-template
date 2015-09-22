71ec69673542fc315471a5c92795d04118c3726b
Huan <huannguyenbach@gmail.com>
2015-09-22 10:01:14 +0700

    uh

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 
-/*Video content Toggle*/
+
     $( document ).ready(function() {
+/*Video content Toggle*/
         $('.content-more').hide();
         $('#content-more').click(function(){
             $('.content-more').toggle('slow');
             $('#content-more').toggleClass('more-toggle');
-        })
-    })
+        });
+
+/*Click to Channels*/
+        $('.dropdown').hide();
+        $('.parent').click(function(event){
+        	event.stopPropagation();
+        	$('.dropdown').slideToggle('slow');
+        });
+        $('.dropdown').on('click', function(event){
+        	event.stopPropagation();
+        });
+    });
+    $(document).on('click', function(){
+    	$('.dropdown').hide();
+    });
 /*-----------------------*/
